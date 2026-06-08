eb8fff4af79ed859d7fa07dc7664251b23f43d98
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:16 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/BoostModule.lua
@@ -0,0 +1,21 @@
+return {
+	["Lucky"] = {
+		Rarity = "Epic",
+		Info = "+50% Luck"
+	},
+	
+	["Speedy"] = {
+		Rarity = "Epic",
+		Info = "+35% Hatch Speed"
+	},
+	
+	["Ultra Lucky"] = {
+		Rarity = "Legendary",
+		Info = "+150% Luck"
+	},
+	
+	["Developer Lucky"] = {
+		Rarity = "Secret",
+		Info = "+1000% Luck"
+	}
+}
