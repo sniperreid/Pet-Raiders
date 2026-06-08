cc64f02f4c1521ea16815c1ef4755fafdad2a500
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:55:25 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Main/TitlesService/Titles.lua
@@ -0,0 +1,43 @@
+return {
+	{
+		Title = "Beginner",
+		Type = "Progress",
+		Description = "???",
+		Color = Color3.fromRGB(100, 190, 255)
+	},
+	
+	{
+		Title = "Novice",
+		Type = "Progress",
+		Description = "???",
+		Color = Color3.fromRGB(255, 155, 55)
+	},
+	
+	{
+		Title = "Experienced",
+		Type = "Progress",
+		Description = "???",
+		Color = Color3.fromRGB(85, 255, 100)
+	},
+	
+	{
+		Title = "Talented",
+		Type = "Progress",
+		Description = "???",
+		Color = Color3.fromRGB(180, 110, 255)
+	},
+	
+	{
+		Title = "Godly",
+		Type = "Progress",
+		ChatTag = true,
+		Description = "???",
+	},
+	
+	{
+		Title = "Developer",
+		Type = "Special",
+		ChatTag = true,
+		Description = "???",
+	}
+}
