b76fe0c44badd2bab9a2ddb95fbafa3795bed379
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:28 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/EggModule/Common Egg.lua
@@ -0,0 +1,14 @@
+return {
+	Cost = {"Coins", 50},
+	Order = 1,
+	IndexReward = "King Doggy",
+	Pets = {
+		{"Doggy", 34},
+		{"Kitty", 29},
+		{"Bunny", 19},
+		{"Bear", 9},
+		{"Overseer", 5},
+		{"King Doggy", 15},
+		{"Solar Deity", 15},
+	}
+}
