091f6ff4f033836bb58456403072ef3c553896fa
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:25 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/BossClass_BLUESHELL/Types.lua
@@ -0,0 +1,17 @@
+local Types = {}
+
+export type Meta = {
+	Model: Model,
+	Player: Player,
+	Name: string,
+	Health: number,
+	MaxHealth: number,
+	Speed: number
+}
+
+export type Data = {
+	Health: number,
+	Speed: number
+}
+
+return Types
