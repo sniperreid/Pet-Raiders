bd7c5f84d6b5f4e0fd496f6d7405e6c7a11eac13
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:55:26 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Main/WheelService/Prizes.lua
@@ -0,0 +1,51 @@
+return {
+	Overworld = {
+		{
+			prize = "Doggy",
+			_type = "Pet",
+			chance = nil -- distributed chance.
+		},
+		
+		{
+			prize = "Clicks",
+			_type = "Currency",
+			amount = 1000,
+			chance = nil -- distributed chance.
+		},
+		{
+			prize = "Doggy",
+			_type = "Pet",
+			chance = nil -- distributed chance.
+		},
+
+		{
+			prize = "Clicks",
+			_type = "Currency",
+			amount = 1000,
+			chance = nil -- distributed chance.
+		},
+		{
+			prize = "Doggy",
+			_type = "Pet",
+			chance = nil -- distributed chance.
+		},
+
+		{
+			prize = "Clicks",
+			_type = "Currency",
+			amount = 1000,
+			chance = nil -- distributed chance.
+		},
+		{
+			prize = "Doggy",
+			_type = "Pet",
+			chance = nil -- distributed chance.
+		},
+
+		{
+			prize = "King Doggy",
+			_type = "Pet",
+			chance = .5
+		}
+	}
+}
