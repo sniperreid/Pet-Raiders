bcb32cf1593f5d2b9ac3852d25fb4cc15812ffb2
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:49 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/RecipeModule/Pets.lua
@@ -0,0 +1,43 @@
+return {
+	["Dogcat"] = {
+		{
+			Name = "Doggy",
+			Amount = 1,
+			Type = "Pet"
+		},
+		
+		{
+			Name = "Kitty",
+			Amount = 1,
+			Type = "Pet"
+		},
+		
+		{
+			Name = "Hallowed Shard",
+			Amount = 1,
+			Type = "Item"
+		}
+	},
+	
+	["Ascended Doggy"] = {
+		{
+			Name = "King Doggy",
+			Amount = 10,
+			Type = "Pet"
+		},
+		
+		{
+			Name = "Hallowed Shard",
+			Amount = 1,
+			Type = "Item"
+		}
+	},
+	
+	["Raven Noire"] = {
+		{
+			Name = "Doggy",
+			Amount = 69,
+			Type = "Pet"
+		}
+	}
+}
