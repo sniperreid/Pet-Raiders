0e59996034e3bc413f6f5452d684d9358d3200de
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:56:29 -0500

    No commit message.

---
+++ ServerScriptService/Modules/Util/PickupService/Ocean.lua
@@ -0,0 +1,29 @@
+return {
+	PickupCount = 50,
+
+	Pickups = {
+		{
+			Name = "Coin",
+			Health = 20_000,
+			Reward = {2_200, "Coins"}
+		},
+
+		{
+			Name = "Coin Stack",
+			Health = 23_500,
+			Reward = {4_200, "Coins"}
+		},
+
+		{
+			Name = "Coin Box",
+			Health = 35_000,
+			Reward = {9_000, "Coins"}
+		},
+
+		{
+			Name = "Coin Chest",
+			Health = 47_000,
+			Reward = {22_500, "Coins"}
+		}
+	}
+}
