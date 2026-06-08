bafe6b44fb41afdce1caa6112764ced409905ac9
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:42 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/PetModule/Desert Egg.lua
@@ -0,0 +1,71 @@
+return {
+	["Burnt Doggy"] = {
+		Buffs = {
+			Coins = 4,
+			Speed = 5.5,
+		},
+		
+		State = "Walk",
+		Rarity = "Common",
+	},
+	
+	["Burnt Deer"] = {
+		Buffs = {
+			Coins = 7,
+			Speed = 6,
+		},
+
+		State = "Walk",
+		Rarity = "Common",
+	},
+	
+	["Burnt Fox"] = {
+		Buffs = {
+			Coins = 12,
+			Speed = 6.5,
+		},
+
+		State = "Walk",
+		Rarity = "Unique",
+	},
+	
+	["Burnt Bear"] = {
+		Buffs = {
+			Coins = 16,
+			Speed = 7.25,
+		},
+
+		State = "Walk",
+		Rarity = "Rare",
+	},
+
+	["Demon"] = {
+		Buffs = {
+			Coins = 28,
+			Speed = 8,
+		},
+
+		State = "Fly",
+		Rarity = "Epic",
+	},
+	
+	["Inferno Dragon"] = {
+		Buffs = {
+			Coins = 50,
+			Speed = 9,
+		},
+
+		State = "Fly",
+		Rarity = "Legendary",
+	},
+	
+	["Scorched Shock"] = {
+		Buffs = {
+			Coins = 5_000,
+			Speed = 70,
+		},
+
+		State = "Fly",
+		Rarity = "Secret",
+	}
+}
