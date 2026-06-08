30d3b37a1ce0052e9e8c9a86088d625e1c2dc0da
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:50 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/SettingModule.lua
@@ -0,0 +1,13 @@
+return {
+	["All Pets"] = {
+		Description = "Toggles the visibility of all pet models!"
+	},
+	
+	["Other Pets"] = {
+		Description = "Toggles the visibility of other people's pet models!"
+	},
+	
+	["Music"] = {
+		Description = "Toggles the sound of in-game music!"
+	}
+}
