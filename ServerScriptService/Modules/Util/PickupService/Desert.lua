d39de2f8f6574908ec12e267259e2dd3f2a14906
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:56:27 -0500

    No commit message.

---
+++ ServerScriptService/Modules/Util/PickupService/Desert.lua
@@ -0,0 +1,30 @@
+return {
+	
+	PickupCount = 50,
+	
+	Pickups = {
+		{
+			Name = "Coin",
+			Health = 600,
+			Reward = {120, "Coins"}
+		},
+
+		{
+			Name = "Coin Stack",
+			Health = 820,
+			Reward = {270, "Coins"}
+		},
+
+		{
+			Name = "Coin Box",
+			Health = 1_100,
+			Reward = {1_100, "Coins"}
+		},
+
+		{
+			Name = "Coin Chest",
+			Health = 1_700,
+			Reward = {1_700, "Coins"}
+		}
+	}
+}
