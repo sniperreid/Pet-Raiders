b81e5be52a82ee5f19e80c7fdf705cf22cf34194
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:55:40 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Util/MathUtility/toInput.lua
@@ -0,0 +1,15 @@
+local inputs = {
+	Enum.KeyCode.One,
+	Enum.KeyCode.Two,
+	Enum.KeyCode.Three,
+	Enum.KeyCode.Four,
+	Enum.KeyCode.Five,
+	Enum.KeyCode.Six,
+	Enum.KeyCode.Seven,
+	Enum.KeyCode.Eight,
+	Enum.KeyCode.Nine,
+}
+
+return function(x)
+	return inputs[x] or Enum.KeyCode.Zero
+end
