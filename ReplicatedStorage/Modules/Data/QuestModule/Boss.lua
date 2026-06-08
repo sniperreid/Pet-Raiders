fa94ff5c11d23a21e7577d41abd493a1fe92e8aa
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:47 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/QuestModule/Boss.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		Name = "Boss1",
+		Requirement = 1,
+		Type = "Boss",
+		Description = "Defeat any boss",
+		Giver = "King Doggy",
+		Rewards = {
+			{
+				Name = "King Doggy",
+				Type = "Pet",
+				Tier = "Normal"
+			},
+			{
+				Name = "Lucky",
+				Type = "Boost",
+				Amount = 3_000
+			}
+		}
+	},
+}
