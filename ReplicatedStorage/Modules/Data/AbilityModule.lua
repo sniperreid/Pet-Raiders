6957ba7acb26bea81191dadf7cca299d575f3ef4
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:15 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/AbilityModule.lua
@@ -0,0 +1,51 @@
+local AbilityModule = {
+	["Lightning"] = {
+		Type = "Instant",
+		Rarity = "Epic",
+		Cooldown = 7
+	},
+	
+	["Fireball"] = {
+		Type = "Verify",
+		Rarity = "Legendary",
+		Cooldown = 5
+	},
+	
+	["Volcano"] = {
+		Type = "Instant",
+		Rarity = "Legendary",
+		Cooldown = 15
+	},
+	
+	["Meteor"] = {
+		Type = "Verify",
+		Rarity = "Legendary",
+		Cooldown = 10
+	},
+	
+	["Heal"] = {
+		Type = "Instant",
+		Rarity = "Legendary",
+		Cooldown = 30
+	},
+	
+	["Bubble"] = {
+		Type = "Instant",
+		Rarity = "Legendary",
+		Cooldown = 20
+	},
+	
+	["Ice Aura"] = {
+		Type = "Instant",
+		Rarity = "Legendary",
+		Cooldown = 13
+	},
+	
+	["Storm"] = {
+		Type = "Instant",
+		Rarity = "Legendary",
+		Cooldown = 8
+	},
+}
+
+return AbilityModule
