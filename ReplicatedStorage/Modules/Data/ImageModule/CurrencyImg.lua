db44deab593a7ee8987441475187f0712da64c02
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:33 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/ImageModule/CurrencyImg.lua
@@ -0,0 +1,7 @@
+return {
+	Coins = "rbxassetid://116834992389209",
+	["MoonCoins"] = "rbxassetid://110762356072537",
+	Gems = "rbxassetid://77754442169247",
+	Robux = "rbxassetid://12765389730",
+	Tickets = "rbxassetid://87225925866134"
+}
