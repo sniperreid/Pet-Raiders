9b483acb80ed4dc790a9253bf71ab36da012ecef
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:34 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/ImageModule/InputsImg.lua
@@ -0,0 +1,70 @@
+return {
+	
+	-- Basic Icons:
+	
+	["Locked"] = "rbxassetid://97432995748099",
+	["Unlocked"] = "rbxassetid://129765936398763",
+	
+	["ZoomIn"] = "rbxassetid://124438994254544",
+	["ZoomOut"] = "rbxassetid://137231502632929",
+	
+	["Checkmark"] = "rbxassetid://92650963643763",
+	
+	-- Inputs:
+	
+	--- XBOX ---
+	
+	["XboxButtonA"] = "rbxassetid://87023717439111",
+	["XboxButtonB"] = "rbxassetid://91071340719673",
+	["XboxButtonX"] = "rbxassetid://123821173515215",
+	["XboxButtonY"] = "rbxassetid://125246788326206",
+	
+	["XboxButtonR1"] = "rbxassetid://107988190264472",
+	["XboxButtonR2"] = "rbxassetid://72047278184535",
+	
+	["XboxButtonL2"] = "rbxassetid://85794357483431",
+	
+	["XboxDPadRight"] = "rbxassetid://74805858420670",
+	["XboxDPadLeft"] = "rbxassetid://86527372075186",
+	["XboxDPadUp"] = "rbxassetid://73263620824472",
+	["XboxDPadDown"] = "rbxassetid://138575896910416",
+	
+	--- XBOX ---
+	
+	--- PLAYSTATION ---
+	
+	["PlaystationButtonA"] = "rbxassetid://75091473961985", 
+	["PlaystationButtonB"] = "rbxassetid://74974627657812",
+	["PlaystationButtonX"] = "rbxassetid://87023717439111",
+	["PlaystationButtonY"] = "rbxassetid://133216577601389",
+	
+	["PlaystationButtonR1"] = "rbxassetid://102205669346374",
+	["PlaystationButtonR2"] = "rbxassetid://119773222056706",
+	
+	["PlaystationButtonL2"] = "rbxassetid://121139497145551",
+	
+	["PlaystationDPadRight"] = "rbxassetid://137889493701379",
+	["PlaystationDPadLeft"] = "rbxassetid://92547697336154",
+	["PlaystationDPadUp"] = "rbxassetid://71257513762160",
+	["PlaystationDPadDown"] = "rbxassetid://80081987722184",
+	
+	--- PLAYSTATION ---
+	
+	--- PC ---
+	
+	["PCQ"] = "rbxassetid://80678971903299",
+	["PCE"] = "rbxassetid://72548013980674",
+	["PCF"] = "rbxassetid://133899517166501",
+	["PCOne"] = "rbxassetid://91753804549333",
+	["PCTwo"] = "rbxassetid://104592372241325",
+	["PCThree"] = "rbxassetid://102081245776181",
+	["PCFour"] = "rbxassetid://132743949361465",
+	
+	["PCMouseButton1"] = "rbxassetid://140509292204397",
+	
+	--- PC ---
+	
+	--- MOBILE ---
+	
+	["MobileTouch"] = "rbxassetid://14935119029"
+}
