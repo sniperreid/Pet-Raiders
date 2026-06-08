9ec12ccafdcc27494d4f83cad26abac68f5871f0
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:42 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/PetModule/Common Egg.lua
@@ -0,0 +1,72 @@
+return {
+	["Doggy"] = {
+		Buffs = {
+			Coins = 2,
+			Speed = 4,
+		},
+		
+		State = "Walk",
+		Rarity = "Common",
+	},
+	
+	["Kitty"] = {
+		Buffs = {
+			Coins = 1.75,
+			Speed = 4.25,
+		},
+
+		State = "Walk",
+		Rarity = "Common",
+	},
+	
+	["Bunny"] = {
+		Buffs = {
+			Coins = 3,
+			Speed = 5,
+		},
+
+		State = "Walk",
+		Rarity = "Unique",
+	},
+	
+	["Bear"] = {
+		Buffs = {
+			Coins = 5,
+			Speed = 6.5,
+		},
+
+		State = "Walk",
+		Rarity = "Rare",
+	},
+	
+	["Overseer"] = {
+		Buffs = {
+			Coins = 35,
+			Speed = 12.5,
+		},
+
+		State = "Fly",
+		Rarity = "Legendary",
+	},
+	
+	["King Doggy"] = {
+		Buffs = {
+			Coins = 1_000,
+			Speed = 35,
+		},
+
+		State = "Walk",
+		Rarity = "Secret",
+	},
+	
+	["Solar Deity"] = {
+		Buffs = {
+			MoonCoins = 1_000,
+			Coins = 2_500,
+			Speed = 65,
+		},
+
+		State = "Fly",
+		Rarity = "Secret",
+	}
+}
