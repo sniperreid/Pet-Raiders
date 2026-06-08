e37325c593af36c8d58963f85451b38dc5286975
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:32 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/ImageModule/BoostsImg.lua
@@ -0,0 +1,7 @@
+return {
+	["Lucky"] = "rbxassetid://107015610333625",
+	["Ultra Lucky"] = "rbxassetid://119411158725781",
+	["Developer Lucky"] = "rbxassetid://112236343720864",
+	
+	["Speedy"] = "rbxassetid://83168403462907"
+}
