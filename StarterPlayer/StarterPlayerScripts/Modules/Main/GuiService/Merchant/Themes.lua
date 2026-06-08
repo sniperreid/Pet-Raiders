f71ae6a9edf2729f476ab3e5c0bf01522d8c598d
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:57:39 -0500

    No commit message.

---
+++ StarterPlayer/StarterPlayerScripts/Modules/Main/GuiService/Merchant/Themes.lua
@@ -0,0 +1,11 @@
+return {
+	["Default"] = {
+		Color3.fromRGB(0, 185, 255),
+		Color3.fromRGB(150, 225, 255)
+	},
+	
+	["Night Market"] = {
+		Color3.fromRGB(75, 75, 75),
+		Color3.fromRGB(90, 90, 90)
+	}
+}
