fdb68e74a51bba18c758463db2f19e50e3c4a7b2
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:44 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/PetModule/Special.lua
@@ -0,0 +1,21 @@
+return {
+	["Blueshell"] = {
+		Buffs = {
+			Coins = 4,
+			Speed = 7,
+		},
+		
+		State = "Walk",
+		Rarity = "Exclusive",
+	},
+	
+	["Raven Noire"] = {
+		Buffs = {
+			Coins = 50,
+			Speed = 15,
+		},
+
+		State = "Fly",
+		Rarity = "Exclusive",
+	},
+}
