300a5b7410d68ad800ed74f5920bb6e714070277
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:43 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/PetModule/Mutations.lua
@@ -0,0 +1,21 @@
+return {
+	["Dogcat"] = {
+		Buffs = {
+			Coins = 5,
+			Speed = 10,
+		},
+		
+		State = "Walk",
+		Rarity = "Mutation",
+	},
+	
+	["Ascended Doggy"] = {
+		Buffs = {
+			Coins = 750,
+			Speed = 50,
+		},
+
+		State = "Fly",
+		Rarity = "Mutation",
+	}
+}
