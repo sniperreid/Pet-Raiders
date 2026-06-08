51a6aad6aaceca07567faa8c7701e914f513c924
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:55:08 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Main/ChallengeService/Challenges.lua
@@ -0,0 +1,14 @@
+return {
+	Daily = {
+		{"onPetHatched", "Doggy", 3, "Easy", 5},
+		{"onCurrencyGained", "Coins", 10_000, "Easy", 10},
+		{"onCurrencyGained", "Coins", 100_000, "Mild", 15},
+		{"onEggsHatched", "Eggs Hatched", 100, "Easy", 10}
+	},
+	
+	Hourly = {
+		{"onPetHatched", "King Doggy", 1, "Insane", 200},
+		{"onCurrencyGained", "Coins", 50_000, "Mild", 13},
+		{"onCurrencyGained", "Coins", 500_000, "Hard", 40}
+	}
+}
