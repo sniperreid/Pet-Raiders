81151cff00bcab49c362a152d8b3f71e7c77f81d
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:56:31 -0500

    No commit message.

---
+++ ServerScriptService/Modules/Util/PickupService/Spawn.lua
@@ -0,0 +1,30 @@
+return {
+	
+	PickupCount = 50,
+	
+	Pickups = {
+		{
+			Name = "Coin",
+			Health = 25,
+			Reward = {10, "Coins"}
+		},
+
+		{
+			Name = "Coin Stack",
+			Health = 75,
+			Reward = {40, "Coins"}
+		},
+
+		{
+			Name = "Coin Box",
+			Health = 200,
+			Reward = {220, "Coins"}
+		},
+
+		{
+			Name = "Coin Chest",
+			Health = 400,
+			Reward = {550, "Coins"}
+		}
+	}
+}
