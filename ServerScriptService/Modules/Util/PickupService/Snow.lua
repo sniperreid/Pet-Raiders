1271fa2123d79b8d563c04d8dbd690bf995187b5
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:56:30 -0500

    No commit message.

---
+++ ServerScriptService/Modules/Util/PickupService/Snow.lua
@@ -0,0 +1,30 @@
+return {
+	
+	PickupCount = 50,
+	
+	Pickups = {
+		{
+			Name = "Coin",
+			Health = 2_000,
+			Reward = {340, "Coins"}
+		},
+
+		{
+			Name = "Coin Stack",
+			Health = 2_500,
+			Reward = {670, "Coins"}
+		},
+
+		{
+			Name = "Coin Box",
+			Health = 3_750,
+			Reward = {2_000, "Coins"}
+		},
+
+		{
+			Name = "Coin Chest",
+			Health = 5_500,
+			Reward = {4_500, "Coins"}
+		}
+	}
+}
