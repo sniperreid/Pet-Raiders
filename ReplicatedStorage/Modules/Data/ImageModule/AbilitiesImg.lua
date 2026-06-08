9c92a8ed8606674dbeda5a5411ecb5d08cd3bd66
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:30 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/ImageModule/AbilitiesImg.lua
@@ -0,0 +1,10 @@
+return {
+	Lightning = "rbxassetid://83168403462907",
+	Fireball = "rbxassetid://73907569430577",
+	Volcano = "rbxassetid://112953351927538",
+	Meteor = "rbxassetid://82595799939464",
+	Heal = "rbxassetid://101003117031699",
+	Bubble = "rbxassetid://84207052813330",
+	["Ice Aura"] = "rbxassetid://138693611398156",
+	Storm = "rbxassetid://74180572326540"
+}
