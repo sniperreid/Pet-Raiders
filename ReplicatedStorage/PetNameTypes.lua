7c3d73339ee1f9f1a1c644b11bc1d421a763d951
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:55:52 -0500

    No commit message.

---
+++ ReplicatedStorage/PetNameTypes.lua
@@ -0,0 +1,3 @@
+export type PetNames = "Ascended Doggy" | "Bear" | "Blueshell" | "Bunny" | "Burnt Bear" | "Burnt Deer" | "Burnt Doggy" | "Burnt Fox" | "Demon" | "Dogcat" | "Doggy" | "Inferno Dragon" | "King Doggy" | "Kitty" | "Overseer" | "Raven Noire" | "Scorched Shock" | "Solar Deity"
+
+return {}
