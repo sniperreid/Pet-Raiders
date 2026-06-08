f2f080b3191e44e65e265fad44ffcc98be8b21f3
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:56:30 -0500

    No commit message.

---
+++ ServerScriptService/Modules/Util/PickupService/Space.lua
@@ -0,0 +1,29 @@
+return {
+	PickupCount = 50,
+
+	Pickups = {
+		{
+			Name = "Coin",
+			Health = 50_000,
+			Reward = {7_500, "Coins"}
+		},
+
+		{
+			Name = "Coin Stack",
+			Health = 58_000,
+			Reward = {15_000, "Coins"}
+		},
+
+		{
+			Name = "Coin Box",
+			Health = 64_000,
+			Reward = {33_000, "Coins"}
+		},
+
+		{
+			Name = "Coin Chest",
+			Health = 80_000,
+			Reward = {80_000, "Coins"}
+		}
+	}
+}
