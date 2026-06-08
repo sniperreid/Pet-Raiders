424e3e9b3cb43082d538a163f061500ab33da356
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:45 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/PrizeModule/Eggs.lua
@@ -0,0 +1,31 @@
+return {
+	{
+		Requirement = 5,
+
+		Reward = {
+			Type = "Boost",
+			Name = "Lucky",
+			Amount = 30 * 60
+		}
+	},
+	
+	{
+		Requirement = 10,
+
+		Reward = {
+			Type = "Boost",
+			Name = "Lucky",
+			Amount = 2 * 60 * 60
+		}
+	},
+	
+	{
+		Requirement = 15,
+
+		Reward = {
+			Type = "Pet",
+			Name = "King Doggy",
+			Tier = "Normal"
+		}
+	}
+}
