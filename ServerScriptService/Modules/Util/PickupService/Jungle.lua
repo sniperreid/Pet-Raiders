2ff8685853aff0a0c179462cb8ab95aa9b292ff4
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:56:28 -0500

    No commit message.

---
+++ ServerScriptService/Modules/Util/PickupService/Jungle.lua
@@ -0,0 +1,30 @@
+return {
+	
+	PickupCount = 50,
+	
+	Pickups = {
+		{
+			Name = "Coin",
+			Health = 7_000,
+			Reward = {660, "Coins"}
+		},
+
+		{
+			Name = "Coin Stack",
+			Health = 8_800,
+			Reward = {1_250, "Coins"}
+		},
+
+		{
+			Name = "Coin Box",
+			Health = 13_000,
+			Reward = {2_200, "Coins"}
+		},
+
+		{
+			Name = "Coin Chest",
+			Health = 18_000,
+			Reward = {5_750, "Coins"}
+		}
+	}
+}
