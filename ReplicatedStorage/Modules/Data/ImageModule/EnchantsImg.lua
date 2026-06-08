2d5e9f96f5553f7dc525f8df30c09b80c0473ca5
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:33 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/ImageModule/EnchantsImg.lua
@@ -0,0 +1,35 @@
+return {
+	
+	-- Luck
+	
+	["Luck I"] = "rbxassetid://132555349827972",
+	["Luck II"] = "rbxassetid://115254567970237",
+	["Luck III"] = "rbxassetid://116228698984816",
+	["Luck IV"] = "rbxassetid://138191229333914",
+	["Luck V"] = "rbxassetid://128131084214207",
+	
+	-- Coins
+	
+	["Coins I"] = "rbxassetid://91941568995758",
+	["Coins II"] = "rbxassetid://119755113324548",
+	["Coins III"] = "rbxassetid://100895583597189",
+	["Coins IV"] = "rbxassetid://130818319105658",
+	["Coins V"] = "rbxassetid://114750235562933",
+	
+	-- Speed
+	
+	["Speed I"] = "rbxassetid://92148511677324",
+	["Speed II"] = "rbxassetid://107722910438575",
+	["Speed III"] = "rbxassetid://74180572326540",
+	["Speed IV"] = "rbxassetid://134245654150149",
+	["Speed V"] = "rbxassetid://132142721696254",
+	
+	-- Misc
+	
+	["Warrior"] = "rbxassetid://76992006890892",
+	["Flash"] = "rbxassetid://104973754346836",
+	["Rich"] = "rbxassetid://74768989986269",
+	["Lottery"] = "rbxassetid://109736862591993",
+	["Raider"] = "rbxassetid://91658104630245"
+	
+}
