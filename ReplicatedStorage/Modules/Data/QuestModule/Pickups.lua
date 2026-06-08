dad491e96ec41066dab0514b39fb000a7d23a1c8
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:47 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/QuestModule/Pickups.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		Name = "Pickups1",
+		Requirement = 10,
+		Type = "Pickups",
+		Description = "Break 25 Pickups",
+		Giver = "King Doggy",
+		Rewards = {
+			{
+				Name = "Doggy",
+				Type = "Pet",
+				Tier = "Normal"
+			},
+			{
+				Name = "Lucky",
+				Type = "Boost",
+				Amount = 300
+			}
+		}
+	},
+}
