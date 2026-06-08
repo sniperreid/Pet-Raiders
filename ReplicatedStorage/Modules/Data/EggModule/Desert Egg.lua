b455b5b809f5e34ef602acd2cd26a460f3e2ba42
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:28 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/EggModule/Desert Egg.lua
@@ -0,0 +1,14 @@
+return {
+	Cost = {"Coins", 500},
+	Order = 2,
+	IndexReward = "Scorched Shock",
+	Pets = {
+		{"Burnt Doggy", 28},
+		{"Burnt Deer", 23},
+		{"Burnt Fox", 18},
+		{"Burnt Bear", 14},
+		{"Demon", 9},
+		{"Inferno Dragon", 5},
+		{"Scorched Shock", .01},
+	}
+}
