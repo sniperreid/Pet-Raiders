a23a8d827d9142970882d5e000ff858bf3f37663
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-08 18:54:36 -0500

    No commit message.

---
+++ ReplicatedStorage/Modules/Data/ImageModule/PetsImg.lua
@@ -0,0 +1,23 @@
+return {
+	-- Common Egg
+	Doggy = "rbxassetid://105716458186041",
+	Kitty = "rbxassetid://136182543524566",
+	Bunny = "rbxassetid://124868139159568",
+	Bear = "rbxassetid://130303421356771",
+	Overseer = "rbxassetid://124799956485057",
+	["King Doggy"] = "rbxassetid://90180258328030",
+	["Solar Deity"] = "rbxassetid://134628402584080",
+	
+	-- Desert Egg
+	["Burnt Doggy"] = "rbxassetid://119124196440746",
+	["Burnt Deer"] = "rbxassetid://73761576862071",
+	["Burnt Fox"] = "rbxassetid://71551279595708",
+	["Burnt Bear"] = "rbxassetid://121803513129635",
+	Demon = "rbxassetid://71888531838305",
+	["Inferno Dragon"] = "rbxassetid://134572455978352",
+	["Scorched Shock"] = "rbxassetid://119992028070509",
+	
+	-- Mutations
+	Dogcat = "rbxassetid://136144841061750",
+	["Ascended Doggy"] = "rbxassetid://129866994329896"
+}
